b0ab955fe632d19dda2c7ae7b3edaf69f7a7fed4
vinjn <vinjn.z@gmail.com>
2021-07-07 16:53:21 +0800

    + deploy.bat

---
+++ intel-gpa-plugin/deploy.bat
@@ -0,0 +1,4 @@
+xcopy /E /I logs %USERPROFILE%\Documents\GPA\python_plugins\logs
+xcopy /E /I plugin_api %USERPROFILE%\Documents\GPA\python_plugins\plugin_api
+xcopy /E /I render_doctor %USERPROFILE%\Documents\GPA\python_plugins\render_doctor
+xcopy /E /I utils %USERPROFILE%\Documents\GPA\python_plugins\utils
